2ba43dda94c1da7a2b7ca4ef24943e940fbb6bd7
Yousef Ayesh <ayesh4002@gmail.com>
2025-10-27 21:44:49 -0400

    Assignment 5

--- Assignment5/api/dependencies/config.py
+++ Assignment5/api/dependencies/config.py
@@ -1,6 +1,6 @@
 class conf:
     host = "localhost"
-    database = "sandwich_maker_api"
+    database = "sandwhich_maker_api"
     port = 3306
     user = "root"
-    password = "rootroot"
+    password = "mamawebaba2"
